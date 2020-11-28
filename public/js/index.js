3b370f66dadb8b25132c329e46bde69411bbd476
Rob Sonter <rob.sonter@newstartdigital.com.au>
2020-11-28 22:54:08 +0800

    main landing page and partials completed

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,18 @@
-function sumit(a, b) {
-  return a + b;
-}
+$(function (){
 
-console.log(sumit(1, 2));
+  //Close Welcome Screen and Open Login Screen
+  $("#loginButton").on("click", function(event){
+    $("#welcomeScreen").css("display", "none");
+    $("#loginScreen").css("display", "inline");
+});
+
+  //Close Welcome Screen and Open Sign Up Screen
+  $("#signupButton").on("click", function(event){
+    $("#welcomeScreen").css("display", "none");
+    $("#signupScreen").css("display", "inline");
+});
+  
+
+
+});
+
