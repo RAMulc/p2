c165bbecef4593a870805d1a67bd3ba5b337d1a3
Rob Sonter <rob.sonter@newstartdigital.com.au>
2020-11-30 18:41:11 +0800

    added public to eslint ignore

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,13 @@
-$(function (){
+$(() => {
+  // Close Welcome Screen and Open Login Screen
+  $('#loginButton').on('click', (event) => {
+    $('#welcomeScreen').css('display', 'none');
+    $('#loginScreen').css('display', 'inline');
+  });
 
-  //Close Welcome Screen and Open Login Screen
-  $("#loginButton").on("click", function(event){
-    $("#welcomeScreen").css("display", "none");
-    $("#loginScreen").css("display", "inline");
+  // Close Welcome Screen and Open Sign Up Screen
+  $('#signupButton').on('click', (event) => {
+    $('#welcomeScreen').css('display', 'none');
+    $('#signupScreen').css('display', 'inline');
+  });
 });
-
-  //Close Welcome Screen and Open Sign Up Screen
-  $("#signupButton").on("click", function(event){
-    $("#welcomeScreen").css("display", "none");
-    $("#signupScreen").css("display", "inline");
-});
-  
-
-
-});
-
