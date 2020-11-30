64a7b8439aa8f14dd37b9b8a3eed16309b09db18
Jamie <Sinclair>
2020-11-30 18:41:53 +0800

    modules for recipe

---
+++ models/ingredients.js
@@ -0,0 +1,9 @@
+module.exports = function (sequelize, DataTypes) {
+    const Ingredient = sequelize.define("ingredients", {
+      name: DataTypes.STRING,
+      spoonacularID: DataTypes.INTEGER,
+      imageLink: DataTypes.STRING
+    });
+    return Ingredient
+  };
+  
---
+++ models/recipe.js
@@ -0,0 +1,10 @@
+module.exports = function (sequelize, DataTypes) {
+    var Recipe = sequelize.define("recipe", {
+      name: DataTypes.STRING,
+      recipeText: DataTypes.TEXT,
+      spoonacularID: DataTypes.INTEGER,
+      imageLink: DataTypes.STRING
+    });
+    return Recipe
+  };
+  
